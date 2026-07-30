3e86a3744bd55e73abc389d0f572b95c541222a0
¨DALVADO¨ <davidenriquealvado@gmail.com>
2026-07-29 21:37:01 -0300

    Configure miloideas.com as primary domain, update site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import vercel from '@astrojs/vercel';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://milo-ideas.vercel.app',
+  site: 'https://miloideas.com',
   output: 'server',
   adapter: vercel(),
   integrations: [
